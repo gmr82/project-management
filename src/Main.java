44ef677b2a5fed9e307a34ac0c2040399ce2e519
gmr82 <gmr@ic.ufal.br>
2022-08-31 13:11:12 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import entities.User;
+
+public class Main
+{
+  public static void main (String[] args)
+  {
+    User user = new User(0, "Gustavo");
+
+    System.out.println(user.toString());
+
+  }
+   
+}
